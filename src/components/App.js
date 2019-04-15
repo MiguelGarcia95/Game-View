0cf7e04872a09d9e3668b435187df9f3ca104b8d
Miguel <mgarcia95951@gmail.com>
2019-04-15 17:44:48 -0500

    setup transitions

--- src/components/App.js
+++ src/components/App.js
@@ -2,33 +2,8 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import styled from "styled-components";
 import {slideInLeft, slideOutRight} from '../utils/pageTransitions';
+import {OverflowPage} from '../utils/styledClasses';
 import './App.css';
-
-const OverflowPage = styled.div`
-  width: 100vw;
-  max-width: 100%;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  position: absolute;
-  color: white;
-  overflow: auto;
-  
-  h1 {
-    font-weight: 300;
-    color: white;
-  }
-
-  h2 {
-    font-weight: 400;
-  }
-  p {
-    font-size: 1rem;
-    margin: 20px auto;
-  }
-`;
 
 const HomePageElm = styled(OverflowPage)`
   &.page-enter {
---
+++ src/utils/styledClasses.js
@@ -0,0 +1,25 @@
+export const OverflowPage = styled.div`
+width: 100vw;
+max-width: 100%;
+min-height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+position: absolute;
+color: white;
+overflow: auto;
+
+h1 {
+  font-weight: 300;
+  color: white;
+}
+
+h2 {
+  font-weight: 400;
+}
+p {
+  font-size: 1rem;
+  margin: 20px auto;
+}
+`;
