568f5b17fe09ac76db95c0eab06eea7084c1276c
Miguel <mgarcia95951@gmail.com>
2019-04-15 11:31:22 -0500

    setup transitions

---
+++ src/components/Test.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import styled, {keyframes} from "styled-components";
+
+export const slideInLeft = keyframes`
+  from {
+    -webkit-transform: translate3d(-100%, 0, 0);
+    transform: translate3d(-100%, 0, 0);
+    visibility: visible;
+  }
+
+  to {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+`;
+export const slideOutLeft = keyframes`
+  from {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  to {
+    visibility: hidden;
+    -webkit-transform: translate3d(-100%, 0, 0);
+    transform: translate3d(-100%, 0, 0);
+  }
+`;
+
+export const slideInRight = keyframes`
+  from {
+    transform: translate3d(100%, 0, 0);
+    visibility: visible;
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+export const slideOutRight = keyframes`
+  from {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  to {
+    visibility: hidden;
+    -webkit-transform: translate3d(100%, 0, 0);
+    transform: translate3d(100%, 0, 0);
+  }
+`;
+
+const OverflowPage = styled.div`
+  width: 100vw;
+  max-width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: absolute;
+  color: white;
+  overflow: auto;
+  
+  h1 {
+    font-weight: 300;
+    color: white;
+  }
+
+  h2 {
+    font-weight: 400;
+  }
+  p {
+    font-size: 1rem;
+    margin: 20px auto;
+  }
+`;
+
+const HomePageElm = styled(OverflowPage)`
+  &.page-enter {
+    animation: ${slideInLeft} 0.7s ease forwards;
+  }
+  &.page-exit {
+    animation: ${slideOutRight} 0.7s ease forwards;
+  }
+`;
+
+class Test extends Component {
+  render() {
+    return (
+      <HomePageElm className="App">
+        <Link to='/'>Home</Link>
+      </HomePageElm>
+    );
+  }
+}
+
+export default Test;
