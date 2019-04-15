2446ca739ef364dad32d227e8558c2b8cad8cd3e
Miguel <mgarcia95951@gmail.com>
2019-04-15 10:17:44 -0500

    setup redux

---
+++ src/reducers/gameReducers.js
@@ -0,0 +1,13 @@
+import * as actionTypes from '../actions/types';
+
+const initialState = {
+};
+
+const gameReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default gameReducer;
