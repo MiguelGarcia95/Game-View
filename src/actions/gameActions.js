e71202857a2410dd5879e16b8332c1447c4c7b32
Miguel <mgarcia95951@gmail.com>
2019-04-16 14:20:59 -0500

    set up actions

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -1,31 +1,17 @@
 import * as actionTypes from '../actions/types';
 import axios from 'axios';
-import {IGDBKEY} from '../apiKeys';
+import {GBAPI} from '../apiKeys';
 
 export const getGames = () => {
   return async (dispatch) => {
-
+    
   }
 }
 
 export const getFeed = () => {
   return async (dispatch) => {
-    
+    const results = await axios.get(`https://www.giantbomb.com/api/games/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name`);
+    console.log(results);
     // 2
-    axios({
-      url: "https://api-v3.igdb.com/feeds",
-      method: 'GET',
-      headers: {
-          'Accept': 'application/json',
-          'user-key': IGDBKEY
-      },
-      data: "fields category,content,created_at,feed_likes_count,feed_video,games,meta,published_at,pulse,slug,title,uid,updated_at,url,user;"
-    })
-      .then(response => {
-          console.log(response.data);
-      })
-      .catch(err => {
-          console.error(err);
-      });
   }
 }
---
+++ src/actions/reviewActions.js
@@ -0,0 +1,1 @@
+// URL: https://www.giantbomb.com/api/reviews/?api_key=[YOUR API KEY]
