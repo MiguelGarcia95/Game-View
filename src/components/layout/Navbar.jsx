da4a1bd4c0dcf632b35754aeef0b53dbc11c77dd
Miguel <mgarcia95951@gmail.com>
2019-04-15 19:47:55 -0500

    setup transitions

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <section className="navbar">
+      <section className="brand"></section>
+      <section className="nav">
+
+      </section>
+    </section>
+  )
+}
+
+export default Navbar;
