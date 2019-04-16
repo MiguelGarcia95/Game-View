56425e175618744f26f0af15eadc79b36f6e1df7
Miguel <mgarcia95951@gmail.com>
2019-04-16 17:19:33 -0500

    add actions

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -10,8 +10,18 @@ export const getGames = () => {
 
 export const getFeed = () => {
   return async (dispatch) => {
-    const results = await axios.get(`https://www.giantbomb.com/api/games/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name`);
-    console.log(results);
-    // 2
+    // const results = await axios.get(`https://www.giantbomb.com/api/games/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name`);
+    const results = await axios.get('https://www.giantbomb.com/api/games',{
+      params: {
+        api_key: GBAPI,
+        format: 'json',
+        limit: '20',
+        sort: 'original_release_date:desc',
+        field_list: 'genres,name'
+      }
+    });
+    console.log(results.data);
   }
 }
+
+// https://www.giantbomb.com/api/games/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name
