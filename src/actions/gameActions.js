0810993a0543f38c3642f142e8ccc1e0b26b6ac2
Miguel <mgarcia95951@gmail.com>
2019-04-16 18:24:49 -0500

    fetch games and reviews

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -10,18 +10,15 @@ export const getGames = () => {
 
 export const getFeed = () => {
   return async (dispatch) => {
-    // const results = await axios.get(`https://www.giantbomb.com/api/games/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name`);
     const results = await axios.get('https://www.giantbomb.com/api/games',{
       params: {
         api_key: GBAPI,
         format: 'json',
         limit: '20',
         sort: 'original_release_date:desc',
-        field_list: 'genres,name'
+        field_list: 'api_detail_url,date_added,deck,description,expected_release_month,expected_release_quarter,expected_release_year,guid,id,image,image_tags,name,number_of_user_reviews,original_game_rating,original_release_date,platforms,site_detail_url'
       }
     });
     console.log(results.data);
   }
 }
-
-// https://www.giantbomb.com/api/games/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name
