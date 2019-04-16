d062f2b1a7cb3ab46050bfe0a4f808999e45f6ef
Miguel <mgarcia95951@gmail.com>
2019-04-16 14:21:43 -0500

    set up actions

--- src/actions/reviewActions.js
+++ src/actions/reviewActions.js
@@ -2,19 +2,10 @@ import * as actionTypes from '../actions/types';
 import axios from 'axios';
 import {GBAPI} from '../apiKeys';
 
-export const getGames = () => {
+export const getReviews = () => {
   return async (dispatch) => {
-    
-  }
-}
-
-export const getFeed = () => {
-  return async (dispatch) => {
-    const results = await axios.get(`https://www.giantbomb.com/api/games/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name`);
+    const results = await axios.get(`https://www.giantbomb.com/api/reviews/?api_key=${GBAPI}&format=json&limit=20&sort=original_release_date:desc&field_list=genres,name`);
     console.log(results);
-    // 2
   }
 }
 
-
-// URL: https://www.giantbomb.com/api/reviews/?api_key=[YOUR API KEY]
