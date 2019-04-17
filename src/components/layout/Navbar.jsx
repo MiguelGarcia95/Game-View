bf393f1aa081ad415c3b5f0f11753243e848b0cc
Miguel <mgarcia95951@gmail.com>
2019-04-16 20:52:34 -0500

    set navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
     <section className="navbar">
       <section className="brand"><p>G</p></section>
       <section className="nav">
-        <NavLink activeClassName='active' to='/'>Home</NavLink>
-        <NavLink activeClassName='active' to='/games'>Games</NavLink>
-        <NavLink activeClassName='active' to='/reviews'>Reviews</NavLink>
-        <NavLink activeClassName='active' to='/franchises'>Franchises</NavLink>
-        <NavLink activeClassName='active' to='/character'>Character</NavLink>
-        <NavLink activeClassName='active' to='/test'>Test</NavLink>
+        <NavLink className='nav-link home' activeClassName='active' to='/'>Home</NavLink>
+        <NavLink className='nav-link games' activeClassName='active' to='/games'>Games</NavLink>
+        <NavLink className='nav-link reviews' activeClassName='active' to='/reviews'>Reviews</NavLink>
+        <NavLink className='nav-link franchises' activeClassName='active' to='/franchises'>Franchises</NavLink>
+        <NavLink className='nav-link characters' activeClassName='active' to='/characters'>Characters</NavLink>
+        <NavLink className='nav-link test' activeClassName='active' to='/test'>Test</NavLink>
       </section>
     </section>
   )
