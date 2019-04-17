51f32a9ae177c24833b741bc2423d0a342ae2218
Miguel <mgarcia95951@gmail.com>
2019-04-17 01:00:25 -0500

    set navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -23,17 +23,19 @@ class Navbar extends React.Component {
   render() {
     const {search} = this.state;
     return (
-      <section className="navbar">
-        <NavLink className='nav-link brand' to='/'>G</NavLink>
-        <NavLink className='nav-link home' activeClassName='active' exact to='/'>Home</NavLink>
-        <NavLink className='nav-link games' activeClassName='active' to='/games'>Games</NavLink>
-        <NavLink className='nav-link reviews' activeClassName='active' to='/reviews'>Reviews</NavLink>
-        <NavLink className='nav-link franchises' activeClassName='active' to='/franchises'>Franchises</NavLink>
-        <NavLink className='nav-link characters' activeClassName='active' to='/characters'>Characters</NavLink>
-        <section className='nav-ham' onClick={this.toggleMenu} ><i className="fas fa-bars fa-2x"></i></section>
-        <section className='nav-search' onClick={this.toggleSearch} ><i className="fas fa-search fa-lg"></i></section>
+      <React.Fragment>
+        <section className="navbar">
+          <NavLink className='nav-link brand' to='/'>G</NavLink>
+          <NavLink className='nav-link home' activeClassName='active' exact to='/'>Home</NavLink>
+          <NavLink className='nav-link games' activeClassName='active' to='/games'>Games</NavLink>
+          <NavLink className='nav-link reviews' activeClassName='active' to='/reviews'>Reviews</NavLink>
+          <NavLink className='nav-link franchises' activeClassName='active' to='/franchises'>Franchises</NavLink>
+          <NavLink className='nav-link characters' activeClassName='active' to='/characters'>Characters</NavLink>
+          <section className='nav-ham' onClick={this.toggleMenu} ><i className="fas fa-bars fa-2x"></i></section>
+          <section className='nav-search' onClick={this.toggleSearch} ><i className="fas fa-search fa-lg"></i></section>
+        </section>
         {search && this.searchBar()}
-      </section>
+      </React.Fragment>
     )
   }
 }
