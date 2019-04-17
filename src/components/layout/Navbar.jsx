cfc6de33778aff98ca02f646a9d8e1e03b5a9ba4
Miguel <mgarcia95951@gmail.com>
2019-04-16 22:01:11 -0500

    set navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,14 +6,13 @@ const Navbar = () => {
   return (
     <section className="navbar">
       <section className="brand"><p>G</p></section>
-      <section className="nav">
-        <NavLink className='nav-link home' activeClassName='active' to='/'>Home</NavLink>
-        <NavLink className='nav-link games' activeClassName='active' to='/games'>Games</NavLink>
-        <NavLink className='nav-link reviews' activeClassName='active' to='/reviews'>Reviews</NavLink>
-        <NavLink className='nav-link franchises' activeClassName='active' to='/franchises'>Franchises</NavLink>
-        <NavLink className='nav-link characters' activeClassName='active' to='/characters'>Characters</NavLink>
-        <NavLink className='nav-link test' activeClassName='active' to='/test'>Test</NavLink>
-      </section>
+      <NavLink className='nav-link home' activeClassName='active' to='/'>Home</NavLink>
+      <NavLink className='nav-link games' activeClassName='active' to='/games'>Games</NavLink>
+      <NavLink className='nav-link reviews' activeClassName='active' to='/reviews'>Reviews</NavLink>
+      <NavLink className='nav-link franchises' activeClassName='active' to='/franchises'>Franchises</NavLink>
+      {/* <NavLink className='nav-link characters' activeClassName='active' to='/characters'>Characters</NavLink> */}
+      <NavLink className='nav-link characters' activeClassName='active' to='/test'>Characters</NavLink>
+      <section className='nav-search'>S</section>
     </section>
   )
 }
