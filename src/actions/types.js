16e085aaac68f62fe5b514f065caa5a66ae38d9d
Miguel <mgarcia95951@gmail.com>
2019-04-17 17:49:19 -0500

    set up base pages

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,6 @@
 export const GET_GAMES = 'GET_GAMES';
 export const GET_HOME_GAMES = 'GET_HOME_GAMES';
+export const GET_HOME_GAME_RELEASES = 'GET_HOME_GAME_RELEASES';
 export const GET_GAME = 'GET_GAME';
 export const GET_REVIEWS = 'GET_REVIEWS';
 export const GET_HOME_REVIEWS = 'GET_HOME_REVIEWS';
