f6b687c7a3df993f62efd149544c5fd5e255baab
Miguel <mgarcia95951@gmail.com>
2019-04-17 01:14:13 -0500

    set navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -32,7 +32,7 @@ class Navbar extends React.Component {
           <NavLink className='nav-link franchises' activeClassName='active' to='/franchises'>Franchises</NavLink>
           <NavLink className='nav-link characters' activeClassName='active' to='/characters'>Characters</NavLink>
           <section className='nav-ham' onClick={this.toggleMenu} ><i className="fas fa-bars fa-2x"></i></section>
-          <section className='nav-search' onClick={this.toggleSearch} ><i className="fas fa-search fa-lg"></i></section>
+          <section className={`nav-search ${search ? 'active' : ''}`} onClick={this.toggleSearch} ><i className="fas fa-search fa-lg"></i></section>
         </section>
         {search && this.searchBar()}
       </React.Fragment>
