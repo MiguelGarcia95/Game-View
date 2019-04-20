f717d162bd7d47879fe20a424ab1278848c4a62d
Miguel <mgarcia95951@gmail.com>
2019-04-19 23:47:27 -0500

    create home page

--- src/components/layout/HomeResults.jsx
+++ src/components/layout/HomeResults.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import {trimString} from '../../utils/functions';
 
-const HomeResults = () => {
+const HomeResults = ({result, type}) => {
   return (
     <section className="content_result" key={result.id}>
       <section className="image" style={imageStyle}></section>
@@ -8,7 +9,7 @@ const HomeResults = () => {
         <section className="name"><p>{result.name}</p></section>
         <section className="description"><p>{result.deck ? trimString(result.deck, 80) : 'N/A'}</p></section>
         <section className="meta">
-          <p>From: {result.user}</p>
+          {type === 'sidebar' ? <p>From: {result.user}</p> : <p>Expected: {result.expected_release_year}</p> }
         </section>
       </section>
     </section>
