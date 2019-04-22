7671880fd64d9eb681c0897dc49ff0f7cb2afcdf
Miguel <mgarcia95951@gmail.com>
2019-04-21 22:38:11 -0500

    display sidebar video

--- src/components/layout/HomeResult.jsx
+++ src/components/layout/HomeResult.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {trimString} from '../../utils/functions';
 
 const HomeResult = ({result, type, setCurrentVideo}) => {
@@ -20,7 +21,13 @@ const HomeResult = ({result, type, setCurrentVideo}) => {
         <section className="name"><p>{result.name}</p></section>
         <section className="description"><p>{result.deck ? trimString(result.deck, 80) : 'N/A'}</p></section>
         <section className="meta">
-          {type === 'sidebar' ? <p>From: {result.user}</p> : <p>Expected: {result.expected_release_year}</p> }
+          {type === 'sidebar' && <p>From: {result.user}</p> }
+          {type === 'content' && (
+            <React.Fragment>
+              <p>Expected: {result.expected_release_year}</p>
+              <Link>Go  <i className="fas fa-arrow-right"></i></Link>
+            </React.Fragment>
+          )}
         </section>
       </section>
     </section>
