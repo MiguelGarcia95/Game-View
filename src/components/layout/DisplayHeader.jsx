cb923ca96ad57f3c6aacf073a0c201d2577827a4
Miguel <mgarcia95951@gmail.com>
2019-04-23 21:33:30 -0500

    sort and setup platform display

---
+++ src/components/layout/DisplayHeader.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import moment from 'moment';
+import {Link} from 'react-router-dom';
+// import './css/home_header.css';
+
+const getDate = (original_date, expected_date) => {
+  if (original_date !== null) {
+    return moment(original_date).format('LL');
+  } else if (expected_date) {
+    return expected_date;
+  } else {
+    return 'N/A';
+  }
+}
+
+
+const displayMetaData = (data) => {
+  return data.map(item => {
+    return <span key={item.id} className="data_span">{item.name}</span>
+  })
+}
+
+const displayPlatforms = (platforms, allPlatforms) => {
+  return platforms.map(item => {
+    return (
+      <section key={item.id} className="platform"><p>{item.name}</p></section>
+    )
+  })
+}
+
+const DisplayHeader = ({game, scrollDown}) => {
+  const imageStyle = {
+    backgroundImage: `url(${game.image.screen_large_url})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center'
+  }
+  return (
+    <section className="display_header">
+      <section className="display_image" style={imageStyle}></section>
+      <section className="display_content">
+        <section className="content_container">
+          <img className="image" src={game.image.small_url} alt={game.name}/>
+          <section className="data">
+            <section className="name"><h1>{game.name}</h1></section>
+            <section className="description"><p>{game.deck}</p></section>
+            <section className="platforms">
+              <p className="title">Available On </p>
+              {game.platforms && displayPlatforms(game.platforms)}
+            </section>
+            <section className="genres">
+              <p className="title">Genres: </p>
+              {game.genres && displayMetaData(game.genres)}
+            </section>
+            <section className="themes">
+              <p className="title">Themes: </p>
+              {game.themes && displayMetaData(game.themes)}
+            </section>
+            <section className="release_date">
+              <p className="title">Release Date: </p>
+              <p className='date'>{getDate(game.original_release_date, game.expected_release_year)}</p>
+            </section>
+          </section>
+        </section>
+      </section>
+      <section className="scroll_down" onClick={() => this.scrollDown()}>
+        <i className="fas fa-caret-down fa-4x"></i>
+      </section>
+    </section>
+  )
+}
+
+export default DisplayHeader;
--- src/components/pages/DisplayGame.jsx
+++ src/components/pages/DisplayGame.jsx
@@ -3,7 +3,8 @@ import {connect} from 'react-redux';
 import moment from 'moment';
 
 import Navbar from '../layout/Navbar';
-import {getGame, getRating} from '../../actions/gameActions';
+import DisplayHeader from '../layout/DisplayHeader';
+import {getGame} from '../../actions/gameActions';
 import {Page} from '../../utils/styledClasses';
 
 import './css/page.css';
@@ -15,26 +16,10 @@ class Games extends React.Component {
     } else if (this.props.game.guid !== this.props.match.params.guid) {
       this.props.getGame(this.props.match.params.guid);
     }
-    this.props.getRating('3065-16');
-    // this.props.getRating(`3065-${this.props.game.original_rating.id}`);
   }
 
   scrollDown = () => {
     this.pageDown.scrollIntoView({behavior: 'smooth'});
-  }
-
-  displayMetaData = (data) => {
-    return data.map(item => {
-      return <span key={item.id} className="data_span">{item.name}</span>
-    })
-  }
-
-  displayPlatforms = (platforms, allPlatforms) => {
-    return platforms.map(item => {
-      return (
-        <section key={item.id} className="platform"><p>{item.name}</p></section>
-      )
-    })
   }
 
   sortPlatforms = (platforms, allPlatforms) => {
@@ -48,62 +33,14 @@ class Games extends React.Component {
     }, []);
   }
 
-  getDate = (original_date, expected_date) => {
-    if (original_date !== null) {
-      return moment(original_date).format('LL');
-    } else if (expected_date) {
-      return expected_date;
-    } else {
-      return 'N/A';
-    }
-  }
-
   render() {
-    const {game, platforms} = this.props;
-    let imageStyle;
-    if (game) {
-      imageStyle = {
-        backgroundImage: `url(${game.image.screen_large_url})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center center'
-      }
-    }
+    const {game} = this.props;
     return (
       <Page className="page app">
         <Navbar />
         {game && (
           <React.Fragment>
-            <section className="display_header">
-              <section className="display_image" style={imageStyle}></section>
-              <section className="display_content">
-                <section className="content_container">
-                  <img className="image" src={game.image.small_url} alt={game.name}/>
-                  <section className="data">
-                    <section className="name"><h1>{game.name}</h1></section>
-                    <section className="description"><p>{game.deck}</p></section>
-                    <section className="platforms">
-                      <p className="title">Available On </p>
-                      {game.platforms && this.displayPlatforms(game.platforms)}
-                    </section>
-                    <section className="genres">
-                      <p className="title">Genres: </p>
-                      {game.genres && this.displayMetaData(game.genres)}
-                    </section>
-                    <section className="themes">
-                      <p className="title">Themes: </p>
-                      {game.themes && this.displayMetaData(game.themes)}
-                    </section>
-                    <section className="release_date">
-                      <p className="title">Release Date: </p>
-                      <p className='date'>{this.getDate(game.original_release_date, game.expected_release_year)}</p>
-                    </section>
-                  </section>
-                </section>
-              </section>
-              <section className="scroll_down" onClick={() => this.scrollDown()}>
-                <i className="fas fa-caret-down fa-4x"></i>
-              </section>
-            </section>
+            <DisplayHeader game={game} scrollDown={this.scrollDown} />
             <div ref={node => this.pageDown = node}></div>
             <section className="page_content game">
             
@@ -117,15 +54,13 @@ class Games extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    game: state.games.game,
-    rating: state.games.rating
+    game: state.games.game
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    getGame: guid => dispatch(getGame(guid)),
-    getRating: guid => dispatch(getRating(guid))
+    getGame: guid => dispatch(getGame(guid))
   }
 }
 
