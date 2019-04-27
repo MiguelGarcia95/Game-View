495b74e95ac4f99bbdd101afc47cea2e7aa1670a
Miguel <mgarcia95951@gmail.com>
2019-04-27 02:21:16 -0500

    display game data

--- src/components/layout/HomeHeader.jsx
+++ src/components/layout/HomeHeader.jsx
@@ -82,7 +82,8 @@ const displayColumns = (games) => {
           <section className="name"><h1>{game.game.name}</h1></section>
           <section className="platform"><h3>Platform: {game.platform.name}</h3></section>
           <section className="date"><p>Release Date: {moment(game.release_date).format('LL')}</p></section>
-          <section className='link'><Link to={`games/game/3030-${game.game.id}`} >Check Out <i className="fas fa-arrow-right fa-lg"></i></Link></section>
+          <Link to={`games/game/3030-${game.game.id}`} ><section className='link'>Check Out <i className="fas fa-arrow-right fa-lg"></i></section></Link>
+          
         </section>
       </section>
     )
