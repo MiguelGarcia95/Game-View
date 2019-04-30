ff851babb5f4f5295f4bf48437c7c3b91b24d8bb
Miguel <mgarcia95951@gmail.com>
2019-04-29 19:42:04 -0500

    set up Franchises

--- src/actions/franchiseActions.js
+++ src/actions/franchiseActions.js
@@ -1,17 +1,16 @@
 import * as actionTypes from '../actions/types';
 import axios from 'axios';
-import moment from 'moment';
 import {GBAPI} from '../apiKeys';
 
-export const getFranchises = () => {
+export const getFranchises = offset => {
   return async (dispatch) => {
     const results = await axios.get('https://www.giantbomb.com/api/franchises',{
       params: {
         api_key: GBAPI,
         format: 'json',
-        limit: '10',
-        // sort: 'date_added:desc',
-        field_list: 'aliases,api_detail_url,date_added,date_last_updated,deck,description,guid,id,image,image_tags,name,site_detail_url'
+        limit: '50',
+        offset: offset,
+        field_list: 'date_added,date_last_updated,deck,description,guid,id,image,name'
       }
     });
     console.log(results)
@@ -30,9 +29,7 @@ export const getFranchise = guid => {
       params: {
         api_key: GBAPI,
         format: 'json',
-        limit: '10',
-        // sort: 'date_added:desc',
-        field_list: 'aliases,api_detail_url,date_added,date_last_updated,deck,description,guid,id,image,image_tags,name,site_detail_url'
+        field_list: 'date_added,date_last_updated,deck,description,guid,id,image,name'
       }
     });
     console.log(results)
