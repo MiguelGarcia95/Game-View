85810b7562f70f4a23ab2f27078d395309476555
Miguel <mgarcia95951@gmail.com>
2019-05-02 13:58:35 -0500

    display review

--- src/components/pages/display/DisplayReview.jsx
+++ src/components/pages/display/DisplayReview.jsx
@@ -147,10 +147,10 @@ class DisplayReview extends React.Component {
               <span className="author">{review.reviewer}</span>
             </section>
             <div ref={node => this.pageTop = node}></div>
-            <section className="page_content game">
+            <section className="page_content review">
 
-              <section className="about" id='about'>
-                {/* <header>About The Game</header> */}
+              <section className="about_review" id='about_review'>
+                <header>The Game</header>
                 {review.description ? <div dangerouslySetInnerHTML={{ __html: review.description }} /> : <h2 className='not_available'>Not Available</h2>}
               </section>
 
