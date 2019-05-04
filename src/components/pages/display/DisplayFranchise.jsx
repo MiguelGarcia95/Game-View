8f8862675cd3d258fa5c14e33ef6304abfbc18a5
Miguel <mgarcia95951@gmail.com>
2019-05-03 21:02:34 -0500

    display franchise

--- src/components/pages/display/DisplayFranchise.jsx
+++ src/components/pages/display/DisplayFranchise.jsx
@@ -28,17 +28,17 @@ class DisplayFranchise extends React.Component {
 
 
   componentDidUpdate() {
-    // let about = document.querySelector('#about_review');
-    // if (about !== null) {
-    //   let links = about.getElementsByTagName('a');
-    //   let lazyImages = about.querySelectorAll('.js-lazy-load-image');
-    //   for (const link of links) {
-    //     link.removeAttribute('href');
-    //   }
-    //   lazyImages.forEach(lazyImage => {
-    //     lazyImage.src = lazyImage.dataset.src
-    //   })
-    // }
+    let about = document.querySelector('#about_review');
+    if (about !== null) {
+      let links = about.getElementsByTagName('a');
+      let lazyImages = about.querySelectorAll('.js-lazy-load-image');
+      for (const link of links) {
+        link.removeAttribute('href');
+      }
+      lazyImages.forEach(lazyImage => {
+        lazyImage.src = lazyImage.dataset.src
+      })
+    }
     
     // if (this.state.currentImage) {
     //   window.addEventListener('keydown', e => {
@@ -144,7 +144,7 @@ class DisplayFranchise extends React.Component {
 
               <section className="about_review" id='about_review'>
                 <header>The Game</header>
-                {/* {review.description ? <div dangerouslySetInnerHTML={{ __html: review.description }} /> : <h2 className='not_available'>Not Available</h2>} */}
+                {franchise.description ? <div dangerouslySetInnerHTML={{ __html: franchise.description }} /> : <h2 className='not_available'>Not Available</h2>}
               </section>
 
               {/* <section className="misc">
