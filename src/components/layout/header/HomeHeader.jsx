f1c048d6582bbe39fac373f436fbc9808b9b140e
Miguel <mgarcia95951@gmail.com>
2019-05-07 01:28:13 -0500

    clean component styling

--- src/components/layout/header/HomeHeader.jsx
+++ src/components/layout/header/HomeHeader.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import {Link} from 'react-router-dom';
 
-// import '../css/home_header.css';
-import '../css/header.css';
+import '../css/home_header.css';
 
 const closeColumns = () => {
   const header = document.querySelectorAll('.home_header_col');
