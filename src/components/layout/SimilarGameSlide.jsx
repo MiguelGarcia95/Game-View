da2ea93414e88e45729e24bf277bf3411469fd47
Miguel <mgarcia95951@gmail.com>
2019-05-07 19:55:32 -0500

    add similar games slider

--- src/components/layout/SimilarGameSlide.jsx
+++ src/components/layout/SimilarGameSlide.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {setBackgroundImage} from '../../utils/functions';
 
-const SimilarGameSlide = ({}) => {
+const SimilarGameSlide = ({game}) => {
+  const imageStyle = setBackgroundImage(game.image.medium_url);
   return(
-    <section></section>
+    <section className="cast_character">
+      <section className="cast_image" style={imageStyle}></section>
+      <Link to={`/games/game/${game.guid}`}>
+        <p className="search_result_movie_link"><i className="fas fa-expand-arrows-alt "></i></p>  
+      </Link>
+      <section className="cast_data">
+        <p className="name" >{game.name}</p>
+      </section>
+    </section>
   )
 }
 
