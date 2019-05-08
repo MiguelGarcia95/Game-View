c48bbcb43d441ebc282f8de05396cdbf478cc5cd
Miguel <mgarcia95951@gmail.com>
2019-05-07 20:30:28 -0500

    show similar games

--- src/components/layout/SimilarGameSlide.jsx
+++ src/components/layout/SimilarGameSlide.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-// import {setBackgroundImage} from '../../utils/functions';
 
 const SimilarGameSlide = ({game}) => {
-  // const imageStyle = setBackgroundImage(game.image.medium_url);
   return(
     <section className="similar_game">
-      {/* <section className="cast_image" style={imageStyle}></section> */}
       <section className="similar_game_image" ></section>
       <Link to={`/games/game/3030-${game.id}`}>
         <p className="name" >{game.name}</p>
