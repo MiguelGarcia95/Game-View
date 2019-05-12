b490aa60c316563777c0bea350e54ac72cb12655
Miguel <mgarcia95951@gmail.com>
2019-05-12 18:10:31 -0500

    make all displays responsive

--- src/components/layout/header/DisplayHeader.jsx
+++ src/components/layout/header/DisplayHeader.jsx
@@ -9,30 +9,34 @@ const DisplayHeader = ({type, content, game}) => {
     const headerImage = setBackgroundImageFixed(game.image.screen_large_url);
     return (
       <section className="display_header" style={headerImage}>
-        <h1>{`${content.game.name} review`}</h1>
-        <section className="data">
-          <section className="score">
-            <p>Score: <span>{content.score} / 5</span></p>
+        <section className="display_header_screen">
+          {/* <h1>{`${content.game.name} review`}</h1> */}
+          <section className="data">
+            <section className="score">
+              <p>Score: <span>{content.score} / 5</span></p>
+            </section>
+            <span className="author"><span>By: </span> {content.reviewer}</span>
+            <span className="date"><span>On: </span> {moment(content.publish_date).format('LL')}</span>
           </section>
-          <span className="author"><span>By: </span> {content.reviewer}</span>
-          <span className="date"><span>On: </span> {moment(content.publish_date).format('LL')}</span>
         </section>
-        {/* <section className="meta">
-        </section> */}
       </section>
     )
   } else if (type === 'franchise') {
     const headerImage = setBackgroundImageFixed(content.image.screen_large_url);
     return (
       <section className='display_header franchise_header' style={headerImage}>
-        <h1>Franchise: <span>{content.name}</span></h1>
+        <section className="display_header_screen">
+          <h1>Franchise: <span>{content.name}</span></h1>
+        </section>
       </section>
     )
   } else {
     return (
       <section className='display_header character_header'>
-        <h1>Character: <span>{content.name}</span></h1>
-        <img src={content.image.small_url} alt={content.name} />
+        <section className="display_header_screen">
+          <h1>Character: <span>{content.name}</span></h1>
+          <img src={content.image.small_url} alt={content.name} />
+        </section>
       </section>
     )
   }
