0a1c27f25ad0a69b6b4f1f776a62928a74959f6d
Miguel <mgarcia95951@gmail.com>
2019-05-13 23:44:51 -0500

    Updates

---
+++ precache-manifest.1aa0672cfb257f2364b7aa1dd894a5c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a865e5bc9b17c817e5",
+    "url": "/Game-View/static/js/runtime~main.ae58509a.js"
+  },
+  {
+    "revision": "12c540a8ca38ecd47201",
+    "url": "/Game-View/static/js/main.3323d12a.chunk.js"
+  },
+  {
+    "revision": "de30342ad28ba9f77c6e",
+    "url": "/Game-View/static/js/2.53880d85.chunk.js"
+  },
+  {
+    "revision": "12c540a8ca38ecd47201",
+    "url": "/Game-View/static/css/main.0ee2b584.chunk.css"
+  },
+  {
+    "revision": "e077e201d646a6b7087f86fe086464b0",
+    "url": "/Game-View/index.html"
+  }
+];
